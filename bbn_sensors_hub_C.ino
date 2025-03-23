52e42962cec9da203f51720dcf394bd582832bec
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-23 15:24:50 -0400

    Update bbn_sensors_hub_C.ino

--- bbn_sensors_hub_C.ino
+++ bbn_sensors_hub_C.ino
@@ -6,6 +6,8 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+#include "PulseGenerator.h"
+
 // Configuration
 #define PCNT_H_LIMIT 10000          // Upper limit for the pulse counter
 #define PCNT_L_LIMIT -1             // Lower limit for the pulse counter
@@ -15,12 +17,6 @@
 #define MEASUREMENT_INTERVAL_MS 250 // Measurement interval in milliseconds
 
 #define LEDC_OUTPUT_GPIO 7
-
-// LEDC (PWM) Configuration
-#define LEDC_TIMER LEDC_TIMER_0
-#define LEDC_MODE LEDC_LOW_SPEED_MODE
-#define LEDC_CHANNEL LEDC_CHANNEL_0
-#define LEDC_DUTY_RESOLUTION LEDC_TIMER_13_BIT // 13-bit resolution for duty cycle
 #define LEDC_FREQUENCY 100                     // Initial frequency in Hz
 
 // Variables
@@ -59,31 +55,6 @@ void pcnt_init() {
   ESP_ERROR_CHECK(pcnt_unit_enable(pcnt_unit));
   ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
   ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
-}
-
-// Function to initialize the LEDC (PWM) module
-void ledc_init() {
-  // Configure LEDC timer
-  ledc_timer_config_t ledc_timer = {
-    .speed_mode = LEDC_MODE,
-    .duty_resolution = LEDC_DUTY_RESOLUTION,
-    .timer_num = LEDC_TIMER,
-    .freq_hz = LEDC_FREQUENCY, // Initial frequency
-    .clk_cfg = LEDC_AUTO_CLK,
-  };
-  ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
-
-  // Configure LEDC channel
-  ledc_channel_config_t ledc_channel = {
-    .gpio_num = LEDC_OUTPUT_GPIO,
-    .speed_mode = LEDC_MODE,
-    .channel = LEDC_CHANNEL,
-    .intr_type = LEDC_INTR_DISABLE,
-    .timer_sel = LEDC_TIMER,
-    .duty = (1 << (LEDC_DUTY_RESOLUTION - 1)), // 50% duty cycle
-    .hpoint = 0,
-  };
-  ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 }
 
 // Function to calculate RPM
