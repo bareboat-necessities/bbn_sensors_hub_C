8dac45b94130a36c64fbf8e8e2d671d9cdfd20a0
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-23 15:36:52 -0400

    Update bbn_sensors_hub_C.ino

--- bbn_sensors_hub_C.ino
+++ bbn_sensors_hub_C.ino
@@ -18,8 +18,6 @@ ReactESP app;
 #include "Max6675.h"
 
 // Configuration
-#define PCNT_H_LIMIT 10000          // Upper limit for the pulse counter
-#define PCNT_L_LIMIT -1             // Lower limit for the pulse counter
 #define PCNT_INPUT_GPIO 8           // GPIO pin for pulse input/output
 #define DEBOUNCE_TIME_NS 40         // Debounce time in nanoseconds
 #define PULSES_PER_REVOLUTION 2     // Number of pulses per engine revolution (PPR)
@@ -31,40 +29,6 @@ ReactESP app;
 // Variables
 int64_t last_time = 0;
 int16_t last_count = 0;
-
-pcnt_unit_handle_t pcnt_unit = NULL;
-
-// Function to initialize the PCNT module
-void pcnt_init() {
-  // Configure PCNT unit
-  pcnt_unit_config_t unit_config = {
-    .low_limit = PCNT_L_LIMIT,
-    .high_limit = PCNT_H_LIMIT,
-  };
-  ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, &pcnt_unit));
-
-  // Configure PCNT channel
-  pcnt_chan_config_t chan_config = {
-    .edge_gpio_num = PCNT_INPUT_GPIO,
-    .level_gpio_num = -1, // No level GPIO
-  };
-  pcnt_channel_handle_t pcnt_chan = NULL;
-  ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_config, &pcnt_chan));
-
-  // Set edge and level actions
-  ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_HOLD));
-  ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_KEEP));
-
-  // Set debounce filter
-  pcnt_glitch_filter_config_t filter_config = {
-    .max_glitch_ns = DEBOUNCE_TIME_NS,
-  };
-  ESP_ERROR_CHECK(pcnt_unit_set_glitch_filter(pcnt_unit, &filter_config));
-
-  ESP_ERROR_CHECK(pcnt_unit_enable(pcnt_unit));
-  ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
-  ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
-}
 
 // Function to calculate RPM
 float calculate_rpm() {
