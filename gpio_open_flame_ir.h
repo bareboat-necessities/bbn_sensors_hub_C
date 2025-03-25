4508b8acd2eec795f1d6c83963378212c59e0757
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-24 21:13:21 -0400

    Update gpio_open_flame_ir.h

--- gpio_open_flame_ir.h
+++ gpio_open_flame_ir.h
@@ -2,25 +2,25 @@
 #define gpio_open_flame_ir_h
 
 const int open_flame_sensorPin = 2; 
+int64_t last_flame_time = 0;
 
 void gpio_open_flame_ir_report() {
   int sensorValue = digitalRead(open_flame_sensorPin); // Read the sensor value
 
   if (sensorValue == LOW) { // If the sensor detects a flame (LOW)
-    Serial.println("Flame Detected!");
-  } else {
-     //Serial.println("No Flame Detected");
+    int64_t now = esp_timer_get_time();
+    if ((now - last_flame_time) > 1000000) {
+      gen_nmea0183_xdr("$BBXDR,S,1,,OPEN_FLAME", 1);
+      last_flame_time = now;
+    }
   }
-
-  //gen_nmea0183_xdr("$BBXDR,G,%.1f,O,Ohms_esp32", resistanceSens.measured_resistance);
 }
 
 void gpio_open_flame_ir_try_init() {
   pinMode(open_flame_sensorPin, INPUT);
-  app.onRepeat(30, []() {
+  app.onRepeat(20, []() {
     gpio_open_flame_ir_report();
   });
 }
 
 #endif
-
