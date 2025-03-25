33c7317c9953e45ef7a5879102992e7719684f62
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-25 07:35:17 -0400

    Update mcu_sensors.h

--- mcu_sensors.h
+++ mcu_sensors.h
@@ -1,7 +1,6 @@
 #ifndef mcu_sensors_h
 #define mcu_sensors_h
 
-/*
 #include "i2c_qmp6988.h"
 #include "i2c_bmp280.h"
 #include "i2c_bme680.h"
@@ -12,7 +11,6 @@
 #include "i2c_sgp30.h"
 #include "i2c_vl53l0x.h"
 #include "i2c_ina219.h"
-*/
 
 #include "gpio_open_flame_ir.h"
 #include "gpio_RPM_sensor.h"
@@ -20,7 +18,6 @@
 #include "gpio_240_30_ohm_sensor.h"
 
 void mcu_sensors_scan() {
-  /*
   i2c_ina219_try_init(&Wire1);
   i2c_ads1115_try_init(&Wire1, G38, G39, 100000UL);
   i2c_ain_4_20ma_try_init(&Wire1, G38, G39, 100000UL);
@@ -31,7 +28,7 @@ void mcu_sensors_scan() {
   i2c_sgp30_try_init();
   i2c_bh1750fvi_tr_try_init();
   i2c_vl53l0x_try_init();
-  */
+
   gpio_open_flame_ir_try_init();
   gpio_RPM_sensor_try_init();
   gpio_240_30_ohm_try_init();
